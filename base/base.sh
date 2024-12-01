fc28945de76eb185dc8dad496be010090742f35b
Yan-Xiangjun <xmascreater@163.com>
2024-12-01 21:41:53 +0800

    添加unrar、unzip

--- base/base.sh
+++ base/base.sh
@@ -5,7 +5,7 @@ sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 sudo apt update && sudo apt upgrade -y
 sudo add-apt-repository -y ppa:apt-fast/stable
 sudo apt install -y apt-fast
-sudo apt-fast install -y build-essential cmake git wget python3-pip python3-venv python-is-python3 fonts-noto-cjk tldr
+sudo apt-fast install -y build-essential cmake git wget python3-pip python3-venv python-is-python3 fonts-noto-cjk tldr unrar unzip
 
 echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$(whoami)
 
