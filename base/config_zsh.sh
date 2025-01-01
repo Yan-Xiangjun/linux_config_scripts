b8de83f5b5f3d17a77f576da64c5e376823ff9a3
Yan-Xiangjun <xmascreater@163.com>
2025-01-01 23:06:51 +0800

    zsh开启通配符

--- base/config_zsh.sh
+++ base/config_zsh.sh
@@ -4,6 +4,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="philips"/' ~/.zshrc
 sudo sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 ~/miniconda3/bin/conda init zsh
+echo 'setopt nonomatch' >> ~/.zshrc
 echo '完成！请重启终端！'
 read -s -n 1 -p "按任意键退出..."
 echo ''
