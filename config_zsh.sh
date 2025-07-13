409c0d93b9ce113ab47f904844885b4943c42d03
Yan-Xiangjun <xmascreater@163.com>
2025-07-13 21:39:13 +0800

    添加ghfast

--- config_zsh.sh
+++ config_zsh.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://ghfast.top/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://ghfast.top/https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="philips"/' ~/.zshrc
 sudo sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 #如果~/miniconda3/bin/conda存在
