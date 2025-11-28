0e2de0b62ee04e3416e8b012b594379249e8fe5c
Yan-Xiangjun <xmascreater@163.com>
2025-11-28 22:11:31 +0800

    禁用oh-my-zsh自动更新

--- config_zsh.sh
+++ config_zsh.sh
@@ -4,8 +4,9 @@ set -x
 
 git clone https://ghfast.top/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://ghfast.top/https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="philips"/' ~/.zshrc
-sudo sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="philips"/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+sed -i "s/# zstyle ':omz:update' mode disabled/zstyle ':omz:update' mode disabled/" ~/.zshrc
 #如果~/miniconda3/bin/conda存在
 if [ -f ~/miniconda3/bin/conda ]; then
     ~/miniconda3/bin/conda init zsh
